8e6c11340a8a697102ec9d69d7100b5fef69ffbe
Juan Paladea <juanpaladea5@gmail.com>
2023-12-28 17:21:54 -0300

    useCart

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -2,40 +2,144 @@ import React from 'react'
 import commerce from '../lib/commerce'
 
 const useCart = () => {
-
+    const [cart, setCart] = useState(null);
+    const [loading, setLoading] = useState(false);
+  
     const getCart = () => {
-        commerce.cart.retrieve().then((cart) => console.log(cart))
-    }
-
+        setLoading(true);
+        return commerce.cart.retrieve()
+        .then((cart) => {
+          setCart(cart);
+          return cart;
+        })
+        .catch((error) => {
+          console.error('Error retrieving cart:', error);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    };
+  
     const refreshCart = () => {
-        commerce.cart.refresh().then((cart) => console.log(cart));
-    }
-
+        setLoading(true);
+        return commerce.cart.refresh()
+        .then((cart) => {
+          setCart(cart);
+          return cart;
+        })
+        .catch((error) => {
+          console.error('Error refreshing cart:', error);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    };
+  
     const addToCart = (productId, q) => {
-        commerce.cart.add(productId, q).then((response) => console.log(response));
-    }
-
+        setLoading(true);
+        return commerce.cart.add(productId, q)
+        .then((response) => {
+          setCart(response.cart);
+          return response.cart;
+        })
+        .catch((error) => {
+          console.error('Error adding to cart:', error);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    };
+  
     const updateLineItemQuantity = (itemId, q) => {
-        commerce.cart.update(itemId, { quantity: q }).then(response => console.log(response));
-    }
-
+        setLoading(true);
+        return commerce.cart.update(itemId, { quantity: q })
+        .then((response) => {
+          setCart(response.cart);
+          return response.cart;
+        })
+        .catch((error) => {
+          console.error('Error updating line item quantity:', error);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    };
+  
     const getCartContent = () => {
-        commerce.cart.contents().then((items) => console.log(items));
-    }
-
+        setLoading(true);
+        return commerce.cart.contents()
+        .then((items) => {
+          console.log(items);
+          return items;
+        })
+        .catch((error) => {
+          console.error('Error getting cart content:', error);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    };
+  
     const removeItemFromCart = (itemId) => {
-        commerce.cart.remove(itemId).then((response) => console.log(response));
-    }
-
+        setLoading(true);
+        return commerce.cart.remove(itemId)
+        .then((response) => {
+          setCart(response.cart);
+          return response.cart;
+        })
+        .catch((error) => {
+          console.error('Error removing item from cart:', error);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    };
+  
     const deleteCart = () => {
-        commerce.cart.delete().then((response) => console.log(response));
-    }
-
+        setLoading(true);
+        return commerce.cart.delete()
+        .then(() => {
+          setCart(null);
+        })
+        .catch((error) => {
+          console.error('Error deleting cart:', error);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    };
+  
     const retrieveCartId = () => {
-        commerce.cart.id().then((cartId) => console.log(cartId));
-    }
-
-    return { getCart, refreshCart, addToCart, updateLineItemQuantity, getCartContent, removeItemFromCart, deleteCart, retrieveCartId } 
-}
-
-export default useCart
+        setLoading(true);
+        return commerce.cart.id()
+        .then((cartId) => {
+          console.log(cartId);
+          return cartId;
+        })
+        .catch((error) => {
+          console.error('Error retrieving cart ID:', error);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    };
+  
+    useEffect(() => {
+      getCart();
+    }, []); // Run once on component mount
+  
+    return {
+      cart,
+      getCart,
+      refreshCart,
+      addToCart,
+      updateLineItemQuantity,
+      getCartContent,
+      removeItemFromCart,
+      deleteCart,
+      retrieveCartId,
+      loading,
+    };
+  };
+  
+  export default useCart;
